ecffa2b22655ea77f93a5a55034f178d513e2e66
annetjohn <annetjohn.john@gmail.com>
2022-01-25 14:49:25 -0500

    adding few lines of code.

--- JavaPractice/src/practiceproblems/PrintPerson.java
+++ JavaPractice/src/practiceproblems/PrintPerson.java
@@ -4,7 +4,10 @@ public class PrintPerson {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 10; 
         System.out.println("Printing details of the person.");
+        System.out.println(i);
+        
 	}
 
 }

774a9db81ab14c6508488f9a0f9376f2bf17271d
annetjohn <annetjohn.john@gmail.com>
2022-02-02 13:46:06 -0500

    GuessGame program added.

--- PracticeProblems/src/practiceproblems/GuessGame.java
+++ PracticeProblems/src/practiceproblems/GuessGame.java
@@ -1,10 +1,59 @@
 package practiceproblems;
 
+import java.util.Scanner;
+
 public class GuessGame {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	  public static void guessingNumberGame()
+	    {
+	        // Scanner Class
+	        Scanner sc = new Scanner(System.in);
+	 
+	        // Generate the random numbers between 1 - 1000.
+	        int number = 1 + (int)(1000 * Math.random());
+	 
+	        // Given K trials
+	        int K = 5;
+	 
+	        int i, guess;
+	 
+	        System.out.println("A number is chosen" + " between 1 to 100." + "Guess the number" + " within 5 trials.");
+	        
+	        // Iterate over K Trials
+	        for (i = 0; i < K; i++) {
+	 
+	            System.out.println("Guess the number:");
+	 
+	            // Take input for guessing
+	            guess = sc.nextInt();
+	 
+	            // If the number is guessed
+	            if (number == guess) {
+	                System.out.println("Congratulations!" + " You guessed the number.");
+	                break;
+	            }
+	            else if (number > guess
+	                     && i != K - 1) {
+	                System.out.println("The number is " + "greater than " + guess);
+	            }
+	            else if (number < guess
+	                     && i != K - 1) {
+	                System.out.println("The number is" + " less than " + guess);
+	            }
+	        }
+	 
+	        if (i == K) {
+	            System.out.println("You have exhausted" + " K trials.");
+	 
+	            System.out.println("The number was " + number);
+	        }
+	    }
+	 
+	    // Driver Code
+	    public static void main(String arg[])
+	    {
+	        // Function Call
+	        guessingNumberGame();
+	    }
 
 }
